c2f2c2464a5ec65e38ee4f8da3b18b4f972a7eb9
DannySuarez <suarezd10@gmail.com>
2019-07-17 17:50:07 -0700

    passing tests

--- test/actors.test.js
+++ test/actors.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -19,34 +19,35 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('can create a studio', () => {
+  it('can create an actor', () => {
     return request(app)
-      .post('/api/v1/studio')
-      .send({ name: 'studio one' })
+      .post('/api/v1/actors')
+      .send({ name: 'Ruby Rose' })
       .then((res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          name: 'studio one',
+          name: 'Ruby Rose',
           __v: 0
         });
       }));
   });
 
   it('can get studios', async() => {
-    const studios = await Studio.create([
-      { name: 'silly studio' },
-      { name: 'studio one' },
-      { name: 'another studio' }
+    const actors = await Actor.create([
+      { name: 'sylvester stallone' },
+      { name: 'antonio banderas' },
+      { name: 'reese witherspoon' }
     ]);
 
     return request(app)
-      .get('/api/v1/studio')
+      .get('/api/v1/actors')
       .then(res => {
-        const studiosJSON = JSON.parse(JSON.stringify(studios));        
-        studiosJSON.forEach(studio => {
-          expect(res.body).toContainEqual({ name: studio.name, _id: studio._id });
+        const actorsJSON = JSON.parse(JSON.stringify(actors));        
+        actorsJSON.forEach(actor => {
+          expect(res.body).toContainEqual({ name: actor.name, _id: actor._id });
         });
       });
   });
+
 
 });
