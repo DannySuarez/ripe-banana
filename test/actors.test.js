a04f408c397c9e27da6982e0121557596e58e8ca
DannySuarez <suarezd10@gmail.com>
2019-07-19 15:41:05 -0700

    can get actor by ID

--- test/actors.test.js
+++ test/actors.test.js
@@ -4,7 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
 const Actor = require('../lib/models/Actor');
+const Film = require('../lib/models/Film');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -49,5 +51,50 @@ describe('app routes', () => {
       });
   });
 
+  it('can get actos by Id', async() => {
+    const studios = await Studio.create([
+      { name: 'test studio' },
+      { name: 'test studio two' }
+    ]);
+    const actors = await Actor.create([
+      { name: 'sylvester stallone', dob: new Date(1967, 7, 26), pob: 'england' },
+      { name: 'antonio banderas', dob: new Date(1960, 7, 10), pob: 'somewhere' },
+      { name: 'reese witherspoon', dob: new Date(1976, 3, 22), pob: 'somewhere' }
+    ]);
+    const films = await Film.create([{
+      title: 'Rocky',
+      studio: studios[0]._id,
+      released: 2002,
+      cast: [{
+        role: 'Main',
+        actor: actors[0]._id
+      }]
+    },
+    {
+      title: 'Zoro',
+      studio: studios[1]._id,
+      released: 2000,
+      cast: [{
+        role: 'Main',
+        actor: actors[1]._id
+      }]
+    }
+    ]);
+
+    return request(app)
+      .get(`/api/v1/actors/${actors[0]._id}`)
+      .then((res => {        
+        expect(res.body).toEqual({
+          name: 'sylvester stallone',
+          dob: actors[0].dob.toISOString(),
+          pob: 'england',
+          films: [{
+            _id: films[0]._id.toString(),
+            title: 'Rocky',
+            released: 2002,
+          }]
+        });
+      }));
+  });
 
 });
