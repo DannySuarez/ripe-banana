0c15efbc82484e928b1071286ebc55e2e6b7552e
DannySuarez <suarezd10@gmail.com>
2019-07-19 10:21:05 -0700

    potential travis fix, commented some code

--- test/studio.test.js
+++ test/studio.test.js
@@ -72,7 +72,7 @@ describe('app routes', () => {
     return request(app)
       .get(`/api/v1/studios/${studios[0]._id}`)
       .then((res => {
-        console.log(res.body);
+        // console.log(res.body);
         expect(res.body).toEqual({
           _id: expect.any(String),
           name: studios[0].name,
